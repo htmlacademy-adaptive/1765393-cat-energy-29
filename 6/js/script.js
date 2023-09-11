c6483773853e0e85e0c902c89dc2016d3a2cf564
Keks <keks@htmlacademy.ru>
2023-09-11 22:45:44 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,14 +1,1 @@
-let navMain = document.querySelector('.main-navigation');
-let navToggle = document.querySelector('.main-navigation__toggle');
-
-navMain.classList.remove('main-navigation--nojs');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-navigation--closed')) {
-    navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--opened');
-  } else {
-    navMain.classList.add('main-navigation--closed');
-    navMain.classList.remove('main-navigation--opened');
-  }
-});
+let navMain=document.querySelector(".main-navigation"),navToggle=document.querySelector(".main-navigation__toggle");function initCatSlider(){const n=document.querySelector(".benefits-example__image-container");document.querySelector(".benefits-example__range-slider")?.addEventListener("input",(e=>{n.style.setProperty("--percent",`${e.target.value}%`),document.querySelector("output").innerHTML=`${e.target.value}%`}))}navMain.classList.remove("main-navigation--nojs"),navToggle.addEventListener("click",(function(){navMain.classList.contains("main-navigation--closed")?(navMain.classList.remove("main-navigation--closed"),navMain.classList.add("main-navigation--opened")):(navMain.classList.add("main-navigation--closed"),navMain.classList.remove("main-navigation--opened"))})),initCatSlider();
