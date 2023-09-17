0b57c4cd3511a726186cca941442b62ffdcbd467
Nina Kun <nk.rabsystem@gmail.com>
2023-09-17 22:50:09 +0300

    Fix typo

--- source/js/script.js
+++ source/js/script.js
@@ -25,28 +25,3 @@ function initCatSlider() {
 }
 
 initCatSlider();
-
-/*
-function checkWebP(callback) {
-  var webP = new Image();
-
-  const event = () => {
-    callback(webP.height === 2);
-  };
-
-  webP.addEventListener("load", event);
-
-  webP.addEventListener("error", event);
-
-  webP.src =
-    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-
-checkWebP(function(support) {
-  if (support) {
-    document.body.classList.add("webp");
-  } else {
-    document.body.classList.add("no-webp");
-  }
-});
-*/
